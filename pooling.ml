19845d7e590a70355c4bb476b37e133763446d5a
Fabien-B <fabien.bonneval@gmail.com>
2015-12-02 00:54:43 +0100

    Add pooling functions.

---
+++ pooling.ml
@@ -0,0 +1,29 @@
+let poolimg = fun poolFct inputImg ->
+  let height = Array.length inputImg in
+  let width = Array.length inputImg.(0) in
+  if height < 2 && width < 2    (* si l'image est trop petite, on la retourne directement *)
+  then
+    inputImg
+  else
+    begin
+    let outputImg = Array.make_matrix (height/2) (width/2) 0 in    
+    for i = 0 to height/2 - 1 do
+      for j = 0 to width/2 - 1 do
+        let pixToPool = [inputImg.(2*i).(2*j) ; inputImg.(2*i+1).(2*j) ; inputImg.(2*i).(2*j+1) ; inputImg.(2*i+1).(2*j+1)] in
+        outputImg.(i).(j) <- poolFct pixToPool;
+      done
+    done;
+      outputImg;
+    end
+
+let sumPooling = fun pixs ->
+  List.fold_left ( + ) 0 pixs
+
+
+let max x y = if x < y then y else x
+let maxPooling = fun pixs ->
+  match pixs with
+    x :: xs -> List.fold_left max x xs
+  | [] -> failwith "maximum of an empty list !\n"
+
+let basicPool = poolimg maxPooling
