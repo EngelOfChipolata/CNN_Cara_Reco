591796c6cc7b0d9c12dfcf631cd3db80ed1e0c16
EngelOfChipolata <engelofdeath@live.fr>
2015-12-10 11:07:11 +0100

    [Create Network] Change l'interface de initReseau

--- createnetwork.mli
+++ createnetwork.mli
@@ -1,5 +1,5 @@
 type intermediateweights = float array array array array
 type intermediateoutput = float array
-val initReseau : Importscans.image array -> int -> intermediateweights
+val initReseau : int -> int -> int -> int -> intermediateweights
 val computeNeurons :
   intermediateweights ->Importscans.image array -> intermediateoutput
