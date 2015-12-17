27aa8800652edb7268278b7ccadc54c0970ebd30
Fabien-B <fabien.bonneval@gmail.com>
2015-12-17 01:06:41 +0100

    finish computeImg & test on few images.

--- computevision.ml
+++ computevision.ml
@@ -7,9 +7,10 @@ type neuronalNetwork = {
 let computeImg = fun img network ->
   let convImgs = Convolutional.convoFactory img network.filterImgs in
   let imgsPooled = Pooling.maxPoolConvImg convImgs in
-  Debug.image_factory "image_test/teteste" imgsPooled;
-  let intValues = Createnetwork.computeNeurons network.inter_weights imgsPooled in
-  intValues
+ (* Debug.image_factory "image_test/teteste" imgsPooled;*)
+  let interValues = Createnetwork.computeNeurons network.inter_weights imgsPooled in
+  let finalValues = Cartoonnetwork.computeNeurons network.final_weights interValues in
+  finalValues
 
 let createNetwork = fun () ->
   let filter_width = 3 in
@@ -23,3 +24,26 @@ let createNetwork = fun () ->
   let finalw = Cartoonnetwork.initReseau nb_neu_inter 10 in
   let network = {filterImgs=filterimgs; inter_weights=interw; final_weights=finalw} in
   network
+
+let () =
+  let net = createNetwork () in
+  let img = Importscans.importimg "Caracteres/1/10.pgm" 28 28 in
+  let values = computeImg img net in
+  Array.iter (fun i -> Printf.printf "%f  " i ) values;
+  Printf.printf "\n";
+
+  let img = Importscans.importimg "Caracteres/1/58.pgm" 28 28 in
+  let values = computeImg img net in
+  Array.iter (fun i -> Printf.printf "%f  " i ) values;
+  Printf.printf "\n\n";
+  
+  let img = Importscans.importimg "Caracteres/8/5.pgm" 28 28 in
+  let values = computeImg img net in
+  Array.iter (fun i -> Printf.printf "%f  " i ) values;
+  Printf.printf "\n";
+
+    
+  let img = Importscans.importimg "Caracteres/8/55.pgm" 28 28 in
+  let values = computeImg img net in
+  Array.iter (fun i -> Printf.printf "%f  " i ) values;
+  Printf.printf "\n\n"
