699b16cf965d945c622563a7aced0a7e0925ce6e
EngelOfChipolata <engelofdeath@live.fr>
2016-01-04 08:03:34 +0100

    [Debug] change le header du fichier pgm

--- debug.ml
+++ debug.ml
@@ -1,6 +1,6 @@
 let image_result = fun file image_out ->
   let oc = open_out file in
-  Printf.fprintf oc "P2 \n#CREATOR: XV Version 3.10a Rev: 12/29/94 (PNG patch 1.0) \n%d %d \n255 \n" (Array.length image_out) (Array.length image_out.(0));
+  Printf.fprintf oc "P2\n#CREATOR: XV Version 3.10a Rev: 12/29/94 (PNG patch 1.0)\n%d %d\n255\n" (Array.length image_out) (Array.length image_out.(0));
   for i = 0 to Array.length image_out - 1 do 
     for j = 0 to Array.length image_out.(0) - 1 do
       Printf.fprintf oc "%d " image_out.(i).(j)
