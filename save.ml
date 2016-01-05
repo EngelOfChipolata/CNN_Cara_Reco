9b7e20741244e86e90369b6f1b3b8ba9f5a28c76
Fabien-B <fabien.bonneval@gmail.com>
2016-01-05 15:05:07 +0100

    Creation du type info_network.

--- save.ml
+++ save.ml
@@ -4,8 +4,7 @@ let save_vector = fun file_channel vector ->
 
 let save_pop = fun filename info pop ->
   let channel = open_out filename in
-  let a,b,c,d,e = info in
-  Printf.fprintf channel "%d\n%d\n%d\n%d\n%d\n\n" a b c d e;
+  Printf.fprintf channel "%d\n%d\n%d\n%d\n%d\n\n" info.Computevision.nbFil info.Computevision.sizeFil info.Computevision.nbInterNeu info.Computevision.sizePooImg info.Computevision.nbEndNeu;
   Printf.fprintf channel "%d\n\n" (Array.length pop);
   Array.iter (fun ind -> save_vector channel ind) pop;
   close_out channel
@@ -31,7 +30,7 @@ let open_pop = fun filename ->
   let _ = input_line channel in
   let nb_ind = int_of_string (input_line channel) in
   let _ = input_line channel in
-  let info = (filter_nb, filter_size, nb_neu_inter, pooled_size, nbNeurFin) in
+  let info = {Computevision.nbFil=filter_nb; Computevision.sizeFil=filter_size; Computevision.nbInterNeu=nb_neu_inter; Computevision.sizePooImg=pooled_size; Computevision.nbEndNeu=nbNeurFin} in
   let nbw = filter_nb * filter_size * filter_size + nb_neu_inter * filter_nb * pooled_size * pooled_size + nb_neu_inter * nbNeurFin in
   let pop = Array.init nb_ind (fun _-> get_ind channel nbw) in
   close_in channel;
