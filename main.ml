dc818299b078e4979013cd42f32541feddca6417
Polo <nicolas.dzieciol@gmail.com>
2016-01-06 11:41:56 +0100

    modif du main

--- main.ml
+++ main.ml
@@ -9,7 +9,7 @@ let darwin = fun () ->
      (inf, Transform.createInlinePopulation inf 10 )
   in
   
-  let img0 = Importscans.importimg "Caracteres/4/10.pgm" 28 28 in
+(*  let img0 = Importscans.importimg "Caracteres/4/10.pgm" 28 28 in
   let img1 = Importscans.importimg "Caracteres/4/58.pgm" 28 28 in
   let img2 = Importscans.importimg "Caracteres/4/5.pgm" 28 28 in
   let img3 = Importscans.importimg "Caracteres/4/3.pgm" 28 28 in
@@ -19,9 +19,10 @@ let darwin = fun () ->
   let img7 = Importscans.importimg "Caracteres/4/18.pgm" 28 28 in
   let img8 = Importscans.importimg "Caracteres/5/18.pgm" 28 28 in
   let img9 = Importscans.importimg "Caracteres/1/18.pgm" 28 28 in
-  let img10 = Importscans.importimg "Caracteres/2/18.pgm" 28 28 in
-  let tab = [|(img0,4); (img1,4); (img2,4); (img3,4); (img4,4); (img5,4); (img6,9); (img7,4); (img8,6); (img9,1); (img10,2)|] in
-  
+    let img10 = Importscans.importimg "Caracteres/2/18.pgm" 28 28 in
+    let tab = [|(img0,4); (img1,4); (img2,4); (img3,4); (img4,4); (img5,4); (img6,9); (img7,4); (img8,6); (img9,1); (img10,2)|] in*)
+  let tab = Learn.getSample 100 in
+  Printf.printf " Winn\n";
   let evalfun = fun net -> Neteval.evalNet Computevision.computeImg tab (Transform.tabToSave net info) in
   let bestweights, pop_finale = Learn.learnFromNothing tab info 25 100 1. 0.5 in
   let best = [|bestweights|] in
