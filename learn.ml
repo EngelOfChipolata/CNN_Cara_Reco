a34788870d262e74df1d81662989375f6fe7c402
Polo <nicolas.dzieciol@gmail.com>
2016-01-07 13:18:45 +0100

    Modifie la fonction qui selectionne les images a apprendre, tire au sort les images et enregistre les numero dans Save/Tirage/"correspondance"

--- learn.ml
+++ learn.ml
@@ -13,22 +13,76 @@ let learnFromFile = fun sample file iter differentialw crossov ->
 
 
     
+
+
 let makeTuple = fun img corresp dir ->
   let imgPath = StringLabels.concat "" [dir ; "/" ;(string_of_int img) ; ".pgm"] in
   let imgI = Importscans.importimg imgPath 28 28 in
   let tup = (imgI, !corresp) in
   tup
 
-let recup = fun img corresp nb ->
+let random = fun loto nbImgs ->
+  let imgSort = ref (Random.int nbImgs) in
+  if loto.(!imgSort) = 1 then loto.(!imgSort) <- 0
+  else
+    begin
+      while (loto.(!imgSort) = 0) do
+        imgSort := Random.int nbImgs
+      done;
+      loto.(!imgSort) <- 0
+    end;
+  !imgSort
+
+let resetTab = fun tab ->
+  for i = 0 to (Array.length tab) - 1 do
+    tab.(i) <- 1
+  done;
+  Array.iter (fun i -> tab.(i) <- 1) tab
+
+let recupDraw = fun tab nb ->
+  let t = Array.make nb 0 in
+  let avancement = ref 0 in
+  for i = 0 to (Array.length tab) - 1 do
+    begin
+      if tab.(i) = 0 then
+        begin
+          t.(!avancement) <- i;
+          avancement := !avancement + 1
+        end
+    end
+  done;
+  avancement := 0;
+  t
+
+let resetAndSave = fun filename loto nb ->
+  let vector = recupDraw loto nb in
+  let channel = open_out filename in
+  Array.iter (fun elt -> Printf.fprintf channel "%d\n" elt) vector;
+  close_out channel;
+  resetTab loto
+
+let recup = fun img corresp nb loto ->
   let i = img mod nb in
   let dir = StringLabels.concat "" ["Caracteres/" ; (string_of_int !corresp)] in
-  let tup = makeTuple i corresp dir in
-  if i = (nb -1 ) then corresp := !corresp + 1;
+  let nbImgs = Array.length (Sys.readdir dir) in
+
+  let imgSort = random loto nbImgs in
+
+  let tup = makeTuple imgSort corresp dir in
+
+  if i = (nb -1 ) then
+    begin
+      let filename = StringLabels.concat "" ["Save/Tirage/" ; (string_of_int !corresp)] in
+      resetAndSave filename loto nb;
+      corresp := !corresp + 1
+    end;
   tup
-  
 
 let getSample = fun nb ->
   let corresp = ref 0 in
   let nbRepart = nb / 10 in
-  let tab = Array.init nb ( fun i -> recup i corresp nbRepart ) in
+  let loto = Array.make 1500 1 in
+  let tab = Array.init nb ( fun i -> recup i corresp nbRepart loto ) in
   tab
+
+
--- main.ml
+++ main.ml
@@ -8,19 +8,6 @@ let darwin = fun () ->
     else
      (inf, Transform.createInlinePopulation inf 10 )
   in
-  
-(*  let img0 = Importscans.importimg "Caracteres/4/10.pgm" 28 28 in
-  let img1 = Importscans.importimg "Caracteres/4/58.pgm" 28 28 in
-  let img2 = Importscans.importimg "Caracteres/4/5.pgm" 28 28 in
-  let img3 = Importscans.importimg "Caracteres/4/3.pgm" 28 28 in
-  let img4 = Importscans.importimg "Caracteres/4/24.pgm" 28 28 in
-  let img5 = Importscans.importimg "Caracteres/4/31.pgm" 28 28 in
-  let img6 = Importscans.importimg "Caracteres/9/18.pgm" 28 28 in
-  let img7 = Importscans.importimg "Caracteres/4/18.pgm" 28 28 in
-  let img8 = Importscans.importimg "Caracteres/5/18.pgm" 28 28 in
-  let img9 = Importscans.importimg "Caracteres/1/18.pgm" 28 28 in
-    let img10 = Importscans.importimg "Caracteres/2/18.pgm" 28 28 in
-    let tab = [|(img0,4); (img1,4); (img2,4); (img3,4); (img4,4); (img5,4); (img6,9); (img7,4); (img8,6); (img9,1); (img10,2)|] in*)
   let tab = Learn.getSample 100 in
   let evalfun = fun net -> Neteval.evalNet Computevision.computeImg tab (Transform.tabToSave net info) in
   let bestweights, pop_finale = Learn.learnFromNothing tab info 25 100 1. 0.5 in
@@ -51,6 +38,3 @@ let () =
   else
     darwin ();
   ignore 3
-
-  
-
