fc9901eaae212148b26570dad4199d9c1b6d9865
EngelOfChipolata <engelofdeath@live.fr>
2016-01-09 17:40:30 +0100

    [Learn] Corrige un faute dans le mli

--- learn.mli
+++ learn.mli
@@ -9,7 +9,7 @@ val learnFromFile :
 
 val makeTuple : int -> 'a ref -> string -> Importscans.image * 'a
 
-val recup : int -> int ref -> int -> Importscans.image * int
+val recup : int -> int ref -> int -> int array -> Importscans.image * int
 
 val getSample : int -> (Importscans.image * int) array
 
