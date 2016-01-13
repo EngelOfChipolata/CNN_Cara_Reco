f1eb8cb47454aa6a52775d7fb009198f19cf86a7
Polo <nicolas.dzieciol@gmail.com>
2016-01-13 08:59:53 +0100

    corrige la fonction de seuil

--- importscans.ml
+++ importscans.ml
@@ -5,13 +5,13 @@ type image = float array array (* Une image est en fait une matrice de nombres (
 let seuil = fun v ->
   let send = ref 0 in
   if v > 100 then send := 255
-  else send := v;
-  v
+  else send := 0;
+  !send
 
       
 let importimg = fun imgpath size -> (*Fonction qui lit une image bitmap de taille size*size et renvoie la matrice associée *)
   let channel = open_in imgpath in
   seek_in channel 73;
-  let result_matrix = Array.init size (fun _ -> Array.init size (fun _ -> float ( (int_of_char (input_char channel)))/.255.)) in
+  let result_matrix = Array.init size (fun _ -> Array.init size (fun _ -> float (seuil (int_of_char (input_char channel)))/.255.)) in
   close_in channel;
   result_matrix;;
