ef6f4fd0c089623f5044f98c245f38e7d27c5813
Fabien-B <fabien.bonneval@gmail.com>
2016-01-23 22:13:49 +0100

    Un petit main pour la route !

--- new/main.ml
+++ new/main.ml
@@ -1,14 +1,12 @@
 open Types
 let () =
   let infos = [Fil (3,5); Pool 2; Line 100; LineFinal 10] in
-  let net = Createnetwork.createNetwork infos 28 in
-  let imgs = Importscans.importimg "../../CNN_Cara_Reco/Caracteres/5/231.pgm" 28 in
-  let result = Computevision.computeVision imgs net in
-  let rere = match result with
-               LineValues line -> line
-             | Imgs _ -> failwith "[ERROR] Imgs reçu au lieu de LineValues"
-             in
-  Array.iter (fun elt -> Printf.printf "%f\n" elt) rere;
-  let sum = Array.fold_left ( +. ) 0. rere in
-  Printf.printf "\nsum = %f\n" sum;;
+
+  let tab = Learn.getSample 100 in
+  let bestweights, pop_finale = Learn.learnFromNothing tab infos 50 1000 1. 0.5 in
+  
+  Save.save_pop "best" infos [|bestweights|];
+  Save.save_pop "poppp" infos pop_finale;
+  Printf.printf "Finish%!" 
+
   
